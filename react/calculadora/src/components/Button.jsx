79a180467b9e1dc231195ff6def7443cbd505fc7
root <root@DESKTOP-E0IL46I.>
2025-06-04 16:40:50 -0300

    display1

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import './Button.css'
 
-const button = props => <button className="button">{props.label}</button>
+const button = props => {
+    let classes = 'button'
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return(
+        <button
+            onClick={e => props.click && props.Click(e.target.label)}
+            className={classes}>
+            {props.label}
+        </button>
+
+    )
+}
+
 export default button
